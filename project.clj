91fd0a9146dadd4af73ceb3d33faaa8a39350a7c
Antony Woods <antony@mastodonc.com>
2017-02-23 14:23:10 +0000

    Add ns

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kixi.log "0.1.1-SNAPSHOT"
+(defproject kixi/kixi.log "0.1.1-SNAPSHOT"
   :description "Logging helpers for the Kixi platform"
   :url "http://mastodonc.com"
   :license {:name "Eclipse Public License"
