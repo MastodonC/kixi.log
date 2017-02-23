4bb2d3923cc39beb639db1a622dad316552a1bdb
Antony Woods <antony@mastodonc.com>
2017-02-23 14:16:05 +0000

    Split exception test

--- test/kixi/log_test.clj
+++ test/kixi/log_test.clj
@@ -57,6 +57,15 @@
       (is (= (:msg result-json) "{:foo \"bar\"} 123"))))
 
   (testing "Exception"
+    (let [result-str  (with-out-str (timbre/info (Exception. "broke!")))
+          result-json (as-json result-str)]
+      (is result-json)
+      (is (string? (:msg result-json)))
+      (is (= (:msg result-json) "broke!"))
+      (is (= (get-in result-json [:exception :type]) "class java.lang.Exception"))
+      (is (= (get-in result-json [:exception :message]) "broke!"))))
+
+  (testing "Exception + message"
     (let [result-str  (with-out-str (timbre/info (Exception. "broke!") "456"))
           result-json (as-json result-str)]
       (is result-json)
