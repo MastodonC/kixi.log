44516e9e1cbe222361c8e3a06f256825d68028c2
Antony Woods <antony@mastodonc.com>
2017-02-27 17:28:06 +0000

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kixi/kixi.log "0.1.2"
+(defproject kixi/kixi.log "0.1.3-SNAPSHOT"
   :description "Logging helpers for the Kixi platform"
   :url "http://mastodonc.com"
   :license {:name "Eclipse Public License"
